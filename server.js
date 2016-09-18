28c0b6fdb752134b91261d03d6408d3de6f48b2f
Chayan Roychoudhury <psivintage.ion@gmail.com>
2016-09-18 12:33:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/articl-eone', function(req,res) { 
+app.get('/articl-one', function(req,res) { 
    res.send('Article one requested and will be served here'); 
 });
 
-app.get('/article-rwo', function(req,res) { 
+app.get('/article-two', function(req,res) { 
    res.send('Article two requested and will be served here'); 
 });
 
