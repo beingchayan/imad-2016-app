c45f8beeab7730a3b61e138a45f6cd5a4d2ff537
Chayan Roychoudhury <psivintage.ion@gmail.com>
2016-09-19 22:55:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,65 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title: 'Article One | Chayan',
+    heading: 'Article One',
+    date: 'September 19,2016',
+    content: ` <p>
+                    This is the content of my article.
+                </p>
+                <p>
+                    In a  step-by-step initiatic process, coordinated and orchestrated by the Higher Self of the soul under the cosmic beings and Ascended Masters who govern the destiny of earth, the soul rises to the heart and merges with the Higher Self. The consciousness that has awareness as the spiritual self, while yet existing in the world, begins the path of Being the Mother. This path is equally difficult to Being the Christ. Like the mass consciousness of the world against the Christ, so too is the mass consciousness against the Mother. The world has raised up the Great Whore image of Mother as the perfect expression of nurturance. When the true Mother Light shines forth to defend truth and Being, bringing compassion, mercy and judgment, her Being is rejected and condemned by the ungodly.
+                </p>`
+    };
+
+
+function createTemplate(data)
+{            
+    var title=data.title;
+    var heading=data.heading;
+    var date=data.date;
+    var content=data.content;
+
+                var htmlTemplate = `
+                <html>
+                    <head> 
+                          <title>
+                             ${title}
+                             </title>
+                    <meta name="viewport" content="width=device-width,initial-scale=1" />
+                    <link href="/ui/style.css" rel="stylesheet" />
+                    </head>
+                        <body>
+                      <div class="container">
+                               <div>
+                            <a href="/">Home</a>
+                        </div>
+                        <hr/>
+                        <h3>
+                            ${heading}
+                            </h3>
+                            <div>
+                                ${date}
+                                </div>
+                            <div>
+                                ${content}
+                            </div>
+                       </div>
+                       </body>
+                </html> 
+                    `;
+        
+        return htmlTemplate;         
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function(req,res) { 
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function(req,res) { 
