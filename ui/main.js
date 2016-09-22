5bd3125fa1c10081c02ca122da8a376cf9fe25b9
Chayan Roychoudhury <psivintage.ion@gmail.com>
2016-09-22 22:29:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,10 @@ console.log('Loaded!');
 // Change
 
 var element = document.getElementById('main-text');
+
+// Move Image
+
+var img = document.getElementbyId('madi');
+img.onclick =  function () {
+    img.style.marginLeft = '100px';
+}
