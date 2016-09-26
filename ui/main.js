b31a39433bc5738c6ceb4d2cb75ffd91a11ee059
Chayan Roychoudhury <psivintage.ion@gmail.com>
2016-09-26 22:29:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,34 @@
-console.log('Loaded!');
+// Counter code
+var button = document.getElementById("counter");
+var counter = 0;
+
+button.onclick = function () {
+    
+    //Make a request to the counter endpoint
+    
+    //Capture the response and store it in a variable
+    
+    //Render the variable in the correct span
+    counter += 1;
+    var span = document.getElementById("count");
+    span.innerHTML = counter.toString();
+}
+
+
+//console.log('Loaded!');
 // Change
 
-var element = document.getElementById('main-text');
+//var element = document.getElementById('main-text');
 
 // Move Image
 
-var img = document.getElementById('madi');
-var marginR = 0;
-function moveLeft() {
-    marginR = marginR +  1;
+//var img = document.getElementById('madi');
+//var marginR = 0;
+//function moveLeft() {
+/*    marginR = marginR +  1;
     img.style.marginRight = marginR + 'px';
 }
 img.onclick =  function () {
     var interval = setInterval(moveLeft,50);
     
-}
+} */
