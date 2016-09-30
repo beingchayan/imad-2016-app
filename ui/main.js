c467ab05b85d7850b48be795b076d06b3b6bd704
Chayan Roychoudhury <psivintage.ion@gmail.com>
2016-09-30 13:33:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,8 @@ button.onclick = function () {
     
     //Create a request object
     var request = new XMLHttpRequest();
+    
+    
     //Capture the response and store it in a variable
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE)
@@ -25,18 +27,18 @@ button.onclick = function () {
    request.send(null);
 
 
+/*
+console.log('Loaded!');
 
-//console.log('Loaded!');
-// Change
 
-//var element = document.getElementById('main-text');
+var element = document.getElementById('main-text');
 
 // Move Image
 
-//var img = document.getElementById('madi');
-//var marginR = 0;
-//function moveLeft() {
-/*    marginR = marginR +  1;
+var img = document.getElementById('madi');
+var marginR = 0;
+function moveLeft() {
+    marginR = marginR +  1;
     img.style.marginRight = marginR + 'px';
 }
 img.onclick =  function () {
